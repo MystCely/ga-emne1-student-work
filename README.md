9f28fb1a560267adc786b862e86c45fcda9a85b4
MystCely <mystsol707@gmail.com>
2026-09-07 20:01:09 +0200

    Add first lesson examples

---
+++ README.md
@@ -0,0 +1,3 @@
+# Python code sessions (GA)
+
+Examples and exercises for Emne 1
---
+++ session-01/02-exercises/01_first_exercise.py
@@ -0,0 +1,8 @@
+first_name = "Steph"
+favorite_language = "C#"
+
+print(first_name)
+print(favorite_language)
+
+first_name = "Celery"
+print(first_name)
